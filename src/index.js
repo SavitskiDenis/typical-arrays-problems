05c7b995b117014b68a2364eb3466031b510b481
Denis Savitskiy <denissavitski@strim-tech.com>
2021-01-31 15:26:11 +0300

    feat: add implementation min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+const isArrayCorrect = array => array && array.length > 0
 
 exports.min = function min (array) {
-  return 0;
+  return isArrayCorrect(array) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return isArrayCorrect(array) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return isArrayCorrect(array) ? array.reduce((sum, el) => sum += el) / array.length : 0;
 }
